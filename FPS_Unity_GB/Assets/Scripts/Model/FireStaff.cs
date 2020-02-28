b8b2185aa689710f6a5f12b3207d182a184701af
Dron Corleone <kefirchik13@gmail.com>
2020-02-28 11:46:26 +0300

    Weapon
    Начал менять все скрипты, связанные с оружием. Адаптирую под магию.

---
+++ FPS_Unity_GB/Assets/Scripts/Model/FireStaff.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class FireStaff : Weapon
+    {
+        public override void Fire()
+        {
+            if (CountAmmunition <= 0) return;
+
+            var Ammo = Instantiate(Ammunition, _barrel.position, _barrel.rotation);
+            Ammo.AddForce(_barrel.forward * _force);
+            CountAmmunition--;
+        }
+    }
+}
