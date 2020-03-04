eb94dc20a4b089194fd1c63687015ac9b16e68c6
Dron Corleone <kefirchik13@gmail.com>
2020-03-04 15:03:56 +0300

    Main menu scripting
    Добавил скрипты главного меню

---
+++ FPS_Unity_GB/Assets/Scripts/UI/CreateInterface.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Отвечает за создания управляющих классов на сцене
+/// </summary>
+public class CreateInterface : MonoBehaviour
+{
+    #region Editor
+    /// <summary>
+    /// Создание главного меню
+    /// </summary>
+    public void CreateMainMenu()
+    {
+        CreateComponent();
+        Clear();
+    }
+    /// <summary>
+    /// Создание игрового UI
+    /// </summary>
+    public void CreateGameMenu()
+    {
+        CreateComponent();
+        Clear();
+    }
+
+    private void Clear()
+    {
+        DestroyImmediate(this);
+    }
+
+    private void CreateComponent()
+    {
+    }
+    #endregion
+}
