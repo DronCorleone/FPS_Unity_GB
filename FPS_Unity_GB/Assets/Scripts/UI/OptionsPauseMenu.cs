565d171c0c3da326a521ac0d1bb0641984446ace
Dron Corleone <kefirchik13@gmail.com>
2020-03-16 17:02:21 +0300

    Меню опций
    Начало работы над меню опций в меню паузы

---
+++ FPS_Unity_GB/Assets/Scripts/UI/OptionsPauseMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsPauseMenu : MonoBehaviour
+{
+    enum OptionsPauseMenuItems
+    {
+        Volume,
+        Back
+    }
+}
